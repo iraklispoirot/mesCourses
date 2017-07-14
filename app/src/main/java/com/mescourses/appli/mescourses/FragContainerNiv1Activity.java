5671ee9aeca7e2a2838ff73b893282d4bada8f0e
iraklispoirot <iraklis.poirot@gmail.com>
2017-07-14 12:45:49 +0200

    doc NLA

---
+++ app/src/main/java/com/mescourses/appli/mescourses/FragContainerNiv1Activity.java
@@ -0,0 +1,13 @@
+package com.mescourses.appli.mescourses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragContainerNiv1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frag_container_niv1);
+    }
+}
